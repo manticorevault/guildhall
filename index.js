cf41bd3393879a2a12809148465ee0adaa5f361a
manticorevault <artur_seilah@hotmail.com>
2021-03-25 11:45:05 -0300

    Updated the index.js to connect to the database using mongoose

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
     type Query{
@@ -9,7 +12,7 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        sayHi: () => "Hello World!"
+        sayHi: () => "Finally testing out GraphQL!"
     }
 }
 
@@ -18,7 +21,12 @@ const server = new ApolloServer({
     resolvers
  });
 
- server.listen({ port: 5000 })
+ mongoose
+    .connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log("MongoDB connected!")
+        return  server.listen({ port: 5000 })
+    })    
     .then(res => {
         console.log(`Server running at ${res.url}`)
     })
