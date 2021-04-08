9067483c913ee0d04c7094c45fb6e94f6f9aec03
manticorevault <artur_seilah@hotmail.com>
2021-04-08 10:45:39 -0300

    Added the models and the resolvers and TypeDefs in the index.

--- index.js
+++ index.js
@@ -2,19 +2,37 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+//Import the schemas
+const Post = require("./models/Post");
+
+//Import MongoDB data from config.js
 const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
-    type Query{
-        sayHi: String!
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+
+    type Query {
+        getPosts: [Post]
     } 
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => "Finally testing out GraphQL!"
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
     }
-}
+};
 
 const server = new ApolloServer({ 
     typeDefs, 
