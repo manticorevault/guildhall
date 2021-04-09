1e480fcddbb01c8a57f314165568669b1c53b8da
manticorevault <artur_seilah@hotmail.com>
2021-04-09 09:49:46 -0300

    Apollo client setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import  ApolloProvider from "./ApolloProvider"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  ApolloProvider,
   document.getElementById('root')
 );
 
