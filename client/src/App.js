970988e9410c6b8cd9c33d3ef2521d024b4066d8
manticorevault <artur_seilah@hotmail.com>
2021-04-09 10:59:23 -0300

    Added routes and navigation and a NavBar semantic-ui component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home"
+import Register from './pages/Register'
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div>
-      <h1>
-        Guildhall
-      </h1>
-    </div>
+    <Router>
+      <Container>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Register() {
+    return (
+        <div>
+            <h1> 
+                This is the register page
+            </h1>
+        </div>
+    )
+}
+
+export default Register;
