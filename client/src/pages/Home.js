dcc1f1185a0a403f9792f076444d6d43daaed067
manticorevault <artur_seilah@hotmail.com>
2021-04-09 14:53:12 -0300

    Created the cards for the posts, a like button and a comments button.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,56 @@
 import React from "react"
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
 
 function Home() {
+    const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS)
+
     return (
-        <div>
-            <h1> 
-                This is the Home page
-            </h1>
-        </div>
+        <Grid columns={3}>
+            <Grid.Row className="page-title">
+                <h1>
+                    Quest board
+                </h1>
+            </Grid.Row>
+
+            <Grid.Row>
+                {loading ? (
+                    <h1> Carregando quests... </h1>
+                ) : (
+                     posts && posts.map(post => (
+                        <Grid.Column key={ post.id } style={{ marginBottom: 20 }}>
+                            <PostCard post={ post } />
+                        </Grid.Column>
+                    )) 
+                )}
+            </Grid.Row>
+        </Grid>
     )
 }
+
+const FETCH_POSTS = gql `
+    {
+        getPosts {
+            id 
+            body
+            createdAt
+            username
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id
+                username
+                createdAt
+                body
+            }
+        }
+    }
+`
 
 export default Home;
